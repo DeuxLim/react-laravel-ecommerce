3e15cb9f3905fb68b9d45bdae84682418d9421ed
deuxlim <limdeux27@gmail.com>
2024-11-05 20:03:34 +0800

    initialized Category factory and seeder

--- api/database/factories/CategoryFactory.php
+++ api/database/factories/CategoryFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\Category;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
@@ -16,8 +18,16 @@ class CategoryFactory extends Factory
      */
     public function definition()
     {
+        // Generate a unique category name
+        $categoryName = $this->faker->unique()->word;
+
         return [
-            //
+            'category' => $categoryName,
+            'slug' => Str::slug($categoryName),
+            'description' => $this->faker->sentence(),
+            'icon' => $this->faker->imageUrl(),
+            'parent_id' => Category::exists() && $this->faker->boolean(50) ? Category::inRandomOrder()->value('id') : null,
+            'sort_order' => $this->faker->numberBetween(0, 100), // Random order for sorting purposes
         ];
     }
 }
