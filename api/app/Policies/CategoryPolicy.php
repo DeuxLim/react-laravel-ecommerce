b2edd08f28498004128f6148f8acc432e2f1063f
deuxlim <limdeux27@gmail.com>
2024-11-09 11:34:18 +0800

    initialized category policy

--- api/app/Policies/CategoryPolicy.php
+++ api/app/Policies/CategoryPolicy.php
@@ -5,6 +5,7 @@ namespace App\Policies;
 use App\Models\Category;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class CategoryPolicy
 {
@@ -18,7 +19,7 @@ class CategoryPolicy
      */
     public function viewAny(User $user)
     {
-        //
+        return in_array($user->role_id, [$user::IS_ADMIN, $user::IS_SELLER]);
     }
 
     /**
@@ -30,7 +31,7 @@ class CategoryPolicy
      */
     public function view(User $user, Category $category)
     {
-        //
+        return in_array($user->role_id, [$user::IS_ADMIN, $user::IS_SELLER]);
     }
 
     /**
@@ -41,7 +42,7 @@ class CategoryPolicy
      */
     public function create(User $user)
     {
-        //
+        return in_array($user->role_id, [$user::IS_ADMIN, $user::IS_SELLER]);
     }
 
     /**
@@ -53,7 +54,7 @@ class CategoryPolicy
      */
     public function update(User $user, Category $category)
     {
-        //
+        return in_array($user->role_id, [$user::IS_ADMIN, $user::IS_SELLER]);
     }
 
     /**
@@ -65,7 +66,7 @@ class CategoryPolicy
      */
     public function delete(User $user, Category $category)
     {
-        //
+        return in_array($user->role_id, [$user::IS_ADMIN, $user::IS_SELLER]);
     }
 
     /**
@@ -77,7 +78,7 @@ class CategoryPolicy
      */
     public function restore(User $user, Category $category)
     {
-        //
+        return in_array($user->role_id, [$user::IS_ADMIN, $user::IS_SELLER]);
     }
 
     /**
@@ -89,6 +90,6 @@ class CategoryPolicy
      */
     public function forceDelete(User $user, Category $category)
     {
-        //
+        return in_array($user->role_id, [$user::IS_ADMIN, $user::IS_SELLER]);
     }
 }
